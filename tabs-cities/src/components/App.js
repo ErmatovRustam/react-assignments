1ebc42140bbe8fa13ea7772ee97ea4d08fe459e6
Rustam Ermatov <ermatov_r@auca.kg>
2020-10-23 09:08:15 +0600

    moviepedia-draft01

--- tabs-cities/src/components/App.js
+++ tabs-cities/src/components/App.js
@@ -5,7 +5,6 @@ class App extends Component {
     constructor(props) {
         super();
         this.state = {
-            activeId: 1,
             tabs: [
                 { id: 1, title: "London", content: "London is the capital city of England." },
                 { id: 2, title: "Paris", content: "Paris is the capital of France." },
@@ -13,12 +12,32 @@ class App extends Component {
                 { id: 4, title: "Phoenix", content: "Phoenix is the capital of Arizona." },
                 { id: 5, title: "Sacramento", content: "Sacramento is the capital of California." },
                 { id: 6, title: "Montgomery", content: "Montgomery is the capital of Alabama." },
-            ]
+            ],
+            activeId: 1
         }
+    }
+
+    onChange = (activeId) => {
+        this.setState({activeId})
     }
     
     render() {
-        return 1;
+        const { tabs, activeId } = this.state;
+        return (
+            <div className = 'App'>
+                <nav>
+                    {
+                        tabs.map(eachTab => {
+                            const color = activeId === eachTab.id ? 'active' : '';                            
+                            return <button key={eachTab.id} onClick={() =>this.onChange(eachTab.id)} id = {color}>{eachTab.title}</button>
+                        })
+                }
+                </nav>
+                <section>
+                    <h1>{tabs[activeId-1].title}</h1>
+                </section>
+            </div>
+        );
     }
-}
+} 
 export default App;
