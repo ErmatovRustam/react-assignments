c8f651d1a448c97423d0f4bc851024547de69356
Rustam Ermatov <ermatov_r@auca.kg>
2020-10-24 21:13:59 +0600

    updated versions

--- tabs-cities/src/components/App.js
+++ tabs-cities/src/components/App.js
@@ -6,12 +6,27 @@ class App extends Component {
         super();
         this.state = {
             tabs: [
-                { id: 1, title: "London", content: "London is the capital city of England." },
-                { id: 2, title: "Paris", content: "Paris is the capital of France." },
-                { id: 3, title: "Tokyo", content: "Tokyo is the capital of Japan." },
-                { id: 4, title: "Phoenix", content: "Phoenix is the capital of Arizona." },
-                { id: 5, title: "Sacramento", content: "Sacramento is the capital of California." },
-                { id: 6, title: "Montgomery", content: "Montgomery is the capital of Alabama." },
+                 
+                {
+                    id: 1, title: "London", content: "London is the capital city of England.",
+                    imgSrc: 'https://imageproxy.themaven.net//https%3A%2F%2Fwww.history.com%2F.image%2FMTYyNDg1MjE3MTI1Mjc5Mzk4%2Ftopic-london-gettyimages-760251843-promo.jpg'
+                },
+                {
+                    id: 2, title: "Paris", content: "Paris is the capital of France.",
+                    imgSrc: 'https://img.theweek.in/content/dam/week/leisure/travel/images/2018/7/21/paris-1.jpg'
+                },
+                {
+                    id: 3, title: "Tokyo", content: "Tokyo is the capital of Japan.",
+            imgSrc: 'https://cdn-image.departures.com/sites/default/files/1576002985/header-tokyo-japan-LUXETOKYO1219.jpg'    },
+                {
+                    id: 4, title: "Phoenix", content: "Phoenix is the capital of Arizona.",
+                imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTGHnMc5iZV7x0MH9o7F5k7xped8SCtSvzU-g&usqp=CAU'},
+                {
+                    id: 5, title: "Sacramento", content: "Sacramento is the capital of California.",
+            imgSrc: 'https://assets.6sigma.us/wp-content/uploads/2015/01/IMG-0003.jpg'    },
+                {
+                    id: 6, title: "Montgomery", content: "Montgomery is the capital of Alabama.",
+            imgSrc: 'https://d.newsweek.com/en/full/1351142/montgomery-alabama-travel.jpg'    },
             ],
             activeId: 1
         }
@@ -36,6 +51,14 @@ class App extends Component {
                 <section>
                     <h1>{tabs[activeId-1].title}</h1>
                 </section>
+
+                <p>
+                    <h1>{tabs[activeId-1].content}</h1>
+                </p>
+
+                <div>
+                    <img id = 'img' src = {tabs[activeId-1].imgSrc} alt = 'city photo' />
+                </div>
             </div>
         );
     }
